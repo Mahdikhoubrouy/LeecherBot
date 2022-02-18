4618ab2a8c8045d5de79e32e0c4c485a148ca20e
miticyber <miticyberi@gmail.com>
2022-02-18 21:58:59 +0330

    edit file main

--- __main__.py
+++ __main__.py
@@ -1,6 +1,8 @@
 from xml.etree.ElementInclude import include
 from pyrogram import Client
 import logging
+import os.path
+import os
 from config import API_id,API_hash
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.WARNING)
@@ -13,6 +15,9 @@ API_HASH = API_hash
 
 if __name__ == "__main__":
     print("Starting Bot ... !")
+    if not os.path.exists('data'):
+        os.mkdir("data")
+
     plugins = dict(root="plugins")
     app = Client(session_name="LeecherBot",api_id=API_ID,api_hash=API_HASH,plugins=plugins)
     app.run()
